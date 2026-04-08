26eed7f1f404e4a4009c3dbce3d8859e0c82e0aa
daniel.fernandeze <daniel.fernandeze@edu.uah.es>
2026-04-08 09:05:37 +0200

    nuevo repositorio 08/04/26

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.google.gson.Gson;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        persona p = new persona("Antonio Moratilla" + 97);
+        String json = gson.toJson(p);
+        System.out.println(json);
+    }
+}
---
+++ src/persona.java
@@ -0,0 +1,20 @@
+public class persona {
+    private String nombre = "John Doe";
+    private int edad = 18;
+
+    public persona(String s) {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+}
