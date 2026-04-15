482457f6f54fb9f828a9683b285fb1729bb63e80
danifdez07 <daniel.fernandeze@edu.uah.es>
2026-04-15 03:37:54 +0200

    changes

--- src/BloqueA/Ejemplo1_7.java
+++ src/BloqueA/Ejemplo1_7.java
@@ -4,7 +4,7 @@ public class Ejemplo1_7 {
     public static void ejecutar() {
         try {
             int[] mesas = {1, 2, 3};
-            System.out.println(mesas[10]); // Error de índice
+            System.out.println(mesas[10]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("[Sección 1.7] Error: Esa mesa no existe.");
         }
--- src/BloqueA/Main.java
+++ src/BloqueA/Main.java
@@ -6,35 +6,29 @@ public class Main {
         System.out.println("   REPORTE DE EJECUCIÓN: PRÁCTICA EXCEPCIONES    ");
         System.out.println("================================================\n");
 
-        // 1.1 Introducción (Manejo de archivos) [cite: 4]
         System.out.println(">>> SECCIÓN 1.1: Lectura de menú");
         Ejemplo1_1.ejecutar();
 
-        // 1.2 Pila de llamadas [cite: 115]
         System.out.println("\n>>> SECCIÓN 1.2: Flujo de preparación");
         Ejemplo1_2.ejecutar();
 
-        // 1.3 Excepciones y Pilas (Propagación) [cite: 175]
         System.out.println("\n>>> SECCIÓN 1.3: Búsqueda del manejador");
         Ejemplo1_3.ejecutar();
 
-        // 1.4 Jerarquía de clases [cite: 200]
+
         System.out.println("\n>>> SECCIÓN 1.4: Captura con clase base Exception");
         Ejemplo1_4.ejecutar();
 
-        // 1.5 Operaciones (throw/throws) [cite: 231]
         System.out.println("\n>>> SECCIÓN 1.5: Verificación de Stock");
         Ejemplo1_5.ejecutar();
 
-        // 1.6 Bloque Finally [cite: 286]
+
         System.out.println("\n>>> SECCIÓN 1.6: Limpieza obligatoria");
         Ejemplo1_6.ejecutar();
 
-        // 1.7 Excepciones comunes de la JVM [cite: 440]
         System.out.println("\n>>> SECCIÓN 1.7: Errores típicos de Java");
         Ejemplo1_7.ejecutar();
 
-        // 1.8 Excepción propia (Personalizada) [cite: 463]
         System.out.println("\n>>> SECCIÓN 1.8: Alerta de cierre de local");
         Ejemplo1_8.ejecutar();
 
